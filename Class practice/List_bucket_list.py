9a37ad90a859a5a1e9a90034edb754ebc30d0dbf
Uluru92 <jorddy.castro@gmail.com>
2024-05-01 13:34:34 -0600

    Creating a tuple and a list

---
+++ Class practice/List_bucket_list.py
@@ -0,0 +1,1 @@
+bucket_list = ["Go to Asia", "Marry my japanesse future wife", "Have a Dalmata", "jump from the sky"]
---
+++ Class practice/tuple_practice.py
@@ -0,0 +1,7 @@
+tup = ("New", "life", "Style")
+print (tup[0][2])
+print (tup[1][3])
+print (tup[2][4])
+
+for word in tup:
+    print(word[(len(word)-1)])
