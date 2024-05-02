f4a350385332ed085883f1d09b95d0eb6268c107
Uluru92 <jorddy.castro@gmail.com>
2024-05-02 11:53:39 -0600

    Editing lists and pointers!

--- Class practice/List_bucket_list.py
+++ Class practice/List_bucket_list.py
@@ -5,3 +5,14 @@ bucket_list.pop(0) #eliminates the first space
 print(bucket_list)
 
 print(bucket_list[1:2]) #returns the space 1 to the 2
+
+a = [1,2,3]
+b = [1,2,3]
+print(a,b)
+
+a[0] = 123
+print(a,b)
+
+c = a
+c[0] = 999
+print(a,b)
