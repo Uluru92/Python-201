c14e0f1dba877e55670e14c4fa4acb0fd4ecec8e
Uluru92 <jorddy.castro@gmail.com>
2024-05-03 23:08:22 -0600

    creating my first personal dictionary

---
+++ Class practice/Dictionary_practice.py
@@ -0,0 +1,8 @@
+my_description = {"name": "Jorddy", "lastName": "Castro", "Age": 32, "Country": "Costa_Rica",("New", "life", "Style"):"tup_1"}
+print("My personal dictionary:", my_description)
+
+my_description["Age"] = 20 # we can change the values, NOT NEVER EVER the key
+print("Accesing the key Age and adding + 1 to it:",my_description["Age"]+1)
+
+my_description["Color_eyes"] = "Blue" #we can add more keys with its own values: NEW ENTRY
+print("My personal dictionary UPDATED:", my_description)
