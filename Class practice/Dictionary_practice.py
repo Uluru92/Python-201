fabf4361152a2d7133ecfedbb7b47ad9f6c98d7b
Uluru92 <jorddy.castro@gmail.com>
2024-05-04 00:07:03 -0600

    iterate over keys and values in dictionaries

--- Class practice/Dictionary_practice.py
+++ Class practice/Dictionary_practice.py
@@ -6,3 +6,15 @@ print("Accesing the key Age and adding + 1 to it:",my_description["Age"]+1)
 
 my_description["Color_eyes"] = "Blue" #we can add more keys with its own values: NEW ENTRY
 print("My personal dictionary UPDATED:", my_description)
+
+for key in my_description:
+    print("show me the keys:", key)
+
+for both in my_description.items():
+    print("show me both key and values:", both)
+
+for k in my_description.keys():
+    print("show me the keys:",k)
+
+for v in my_description.values():
+    print("show me the values:",v)
