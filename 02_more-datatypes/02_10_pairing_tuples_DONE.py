50a7d1ba169c18b1356122783f443b83eba93c37
Uluru92 <jorddy.castro@gmail.com>
2024-05-21 22:05:39 -0600

    update

--- 02_more-datatypes/02_10_pairing_tuples_DONE.py
+++ 02_more-datatypes/02_10_pairing_tuples_DONE.py
@@ -1,6 +1,6 @@
 # The import below gives you a new random list of numbers,
 # called `randlist`, every time you run the script.
-#
+
 # Write a script that takes this list of numbers and:
 #     - sorts the numbers
 #     - stores the numbers in tuples of two in a new list
@@ -8,7 +8,7 @@
 #
 # If the list has an odd number of items,
 # add the last item to a tuple together with the number `0`.
-#
+
 # Note: This lab might be challenging! Make sure to discuss it 
 # with your mentor or chat about it on our forum.
 # Write your code below here:
@@ -24,7 +24,6 @@ print("random list:",list_)
 print("list sorted:",list_sorted)
 lenght = list_sorted.__len__()
 print("La longitud de la lista es:", lenght)
-
 
 for i in list_sorted:
     print("posición:",list_sorted.index(i),", igual a:", i)
@@ -54,4 +53,5 @@ for i in list_sorted:
             tuple_+= (i,)
             tuple_+= (0,)
             new_list.append(tuple_)
+
 print("List with tuples of two:", new_list)
