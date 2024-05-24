c2a4927e32d06822d14e0b7920486b7a58ffaf56
Uluru92 <jorddy.castro@gmail.com>
2024-05-24 14:31:30 -0600

    exercise 2.19 DONE

--- 02_more-datatypes/02_19_comprehension_syntax_DONE.py
+++ 02_more-datatypes/02_19_comprehension_syntax_DONE.py
@@ -1,8 +1,17 @@
 # Use a list comprehension to create a list that contains the individual
 # letters of the word "codingnomads":
 # ['c', 'o', 'd', 'i', 'n', 'g', 'n', 'o', 'm', 'a', 'd', 's']
-#
-# Note: You can solve this more quickly with a call to `list()`
-# but try to do it using a list comprehension.
 
 word = "codingnomads"
+print("word:",word)
+
+# Note: You can solve this more quickly with a call to `list()`
+word_list = list(word)
+print("letters list 1:",word_list)
+
+# but try to do it using a list comprehension.
+word_list_2 = list()
+for i in word:
+    word_list_2.append(i)
+
+print("letters list 2:",word_list_2)
