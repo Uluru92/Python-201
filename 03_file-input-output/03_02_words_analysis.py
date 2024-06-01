340299962e035ad0b432322794e491db40267859
Uluru92 <jorddy.castro@gmail.com>
2024-06-01 15:12:20 -0600

    excercise 3.2 update min word found!

--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -5,7 +5,6 @@
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
 
-
 #... dejaré el pseudocógido en mi mente en segundo plano por ahora pa
 #... primero que todo... necesito abrir el archivo
 #... segundo que todo... necesito hacer los filtros:
@@ -15,3 +14,17 @@
 #... contar todas las palabras de la lista indiferentemente de su longitud
 #... nos vemos mañana pa
 #... hoy seguire en 2do plano... desarrollando en offline mode pa
+
+from pathlib import Path
+file_words_path = Path("Python-201-main/03_file-input-output/words.txt")
+words = []
+word_len = int(1000)
+new_shortest_len = int()
+
+with open(file_words_path, "r") as file_words:
+    for word in file_words.readlines():
+        if word_len > word.__len__():
+            new_shortest_len = word.__len__()
+            print("new shortest:", word, " with len:",new_shortest_len)
+            word_len = new_shortest_len
+
