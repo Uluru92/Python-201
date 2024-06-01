e416e5dab4f14bedb1bd0eac1fc9b9e5b7c9377e
Uluru92 <jorddy.castro@gmail.com>
2024-06-01 17:56:33 -0600

    excercise update 3.2

--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -6,7 +6,6 @@
 # 3. The total number of words in the file.
 
 #... segundo que todo... necesito hacer los filtros:
-#... buscar la longitud más corta...
 #... buscar todas las palabras con esa longitud minima
 #... hacer lo mismo con la longitud máxima
 #... contar todas las palabras de la lista indiferentemente de su longitud
@@ -15,13 +14,25 @@
 
 from pathlib import Path
 file_words_path = Path("Python-201-main/03_file-input-output/words.txt")
-words = []
-word_len = int(1000)
+shortest_words = []
+shortest_len = int(1000)
 new_shortest_len = int()
 
 with open(file_words_path, "r") as file_words:
     for word in file_words.readlines():
-        if word_len > word.__len__():
+        word = word.strip()
+        if shortest_len > word.__len__():
             new_shortest_len = word.__len__()
             print("new shortest:", word, " with len:",new_shortest_len)
-            word_len = new_shortest_len
+            shortest_len = new_shortest_len
+
+with open(file_words_path, "r") as file_words:
+    for word in file_words.readline():
+        print("new shortest:", word, " with len:",new_shortest_len)
+        word = word.strip()
+        if word.__len__() == shortest_len:
+            shortest_words.append(word)
+
+print("Shortest words list:")
+for words in shortest_words:
+    print(words)
