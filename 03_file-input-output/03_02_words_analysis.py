93bd23f211d1cf7a6cd2fc8a7615ff2f0fdab5f7
Uluru92 <jorddy.castro@gmail.com>
2024-05-31 20:39:46 -0600

    pseudocódigo ejercicio 3.2 update

--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,13 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+
+#... dejaré el pseudocógido en mi mente en segundo plano por ahora pa
+#... primero que todo... necesito abrir el archivo
+#... segundo que todo... necesito hacer los filtros:
+#... buscar la longitud más corta...
+#... buscar todas las palabras con esa longitud minima
+#... hacer lo mismo con la longitud máxima
+#... contar todas las palabras de la lista indiferentemente de su longitud
+#... nos vemos mañana pa
