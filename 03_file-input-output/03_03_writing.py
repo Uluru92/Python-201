a03c4a524cf5097579b429cb181797b2ff2c3f0a
Uluru92 <jorddy.castro@gmail.com>
2024-06-03 13:46:56 -0600

    pseudocodigo 3.3 update

--- 03_file-input-output/03_03_writing.py
+++ 03_file-input-output/03_03_writing.py
@@ -1,2 +1,7 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+
+#pseudocodigo:
+#abrir el archivo... en modo r... guardar todas las lineas en una lista...
+#abrir un nuevo documento... en modo w... y empujarle todas palabras de la lista aldervez
+
