2f755a7205b92bb5d72a337a0428f78fae51c77e
Uluru92 <jorddy.castro@gmail.com>
2024-06-03 19:20:10 -0600

    ejercicios tema 3 in progress update...

--- 03_file-input-output/03_05_more-filetypes.py
+++ 03_file-input-output/03_05_more-filetypes.py
@@ -2,3 +2,22 @@
 # in your CSV file. Remember that the format of your output needs to be
 # consistent across multiple runs of your script. This means you'll need to
 # make a compromise and choose which file types you want to record beforehand.
+
+import csv  #cvs stands for comma-separated values
+from pathlib import Path
+filecounts_path = Path("Python-201-main/03_file-input-output/filecounts.csv")
+# -- snip --
+count = {"": 8, ".csv": 2, ".md": 2, ".png": 11}
+
+with open(filecounts_path, "w") as csvfile:
+    countwriter = csv.writer(csvfile)
+    data = [count[""], count[".csv"], count[".md"], count[".png"]]
+    countwriter.writerow(data)
+
+with open(filecounts_path, "r") as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=["Folder", "CSV", "MD", "PNG"])
+    counts = list(reader)
+
+print(counts)
+print(type(count))
+print(count.__len__())
