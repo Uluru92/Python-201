a4cee97fd1ade92d8d13d97c08ff910d4c92a459
Uluru92 <jorddy.castro@gmail.com>
2024-06-05 00:08:20 -0600

    progress exercise 4.1

--- 04_functions-and-scopes/04_01_fix_function_definition.py
+++ 04_functions-and-scopes/04_01_fix_function_definition.py
@@ -3,6 +3,11 @@
 # After your fixes, the function should allow you to take a name as an input
 # and return a greeting message that you can save to a variable.
 
+#pseudocodigo... posibles errores:
+#SyntaxError: invalid syntax: significa que el codigo ni siquiera inicia por errores de formato
+#existe un return y no tenemos una funcion definida
+
+
 function say_hello("name"):
         return print(f"Hello {name}!")
 
