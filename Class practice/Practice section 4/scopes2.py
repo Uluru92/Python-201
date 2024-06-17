b5f3bd3a7b3d8d1ec1b8b4f525f026cfdcf30529
Uluru92 <jorddy.castro@gmail.com>
2024-06-16 21:19:57 -0600

    more scope modifying!

--- Class practice/Practice section 4/scopes2.py
+++ Class practice/Practice section 4/scopes2.py
@@ -1,30 +1,13 @@
-name = "Mycroft"
-
+name = "Name1"
 def print_name_box():
     print(name)
-
+    name = "Name2"
     def smaller_box():
-        # (Re)assigning a variable within a local scope
-        # overwrites the same variable from an outer scope
-        # You also can't use the global variable *before*
-        # assigning it, if you assign it anywhere in that scope.
-
-        # --TASK--: uncomment the print() function below
-        #     and interpret the results when running the script
-
-        # print(name)
-        name = "Sherlock"
-
+        name = "Name3"
+        print(name)
         def smallest_box():
-            # Inner scopes always draw from the next outer layer.
-            # After `name` got overwritten, the name that will
-            # be printed is NOT the global-scope name anymore
-            name = "Fabio"
+            name = "Name3"
             print(name)
-            
-
         smallest_box()
-
     smaller_box()
-
 print_name_box()
