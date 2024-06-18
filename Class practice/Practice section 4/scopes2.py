5fc184a2a2a269b8e4dbcc3ea01e72a7cc3dfcc7
Uluru92 <jorddy.castro@gmail.com>
2024-06-17 20:48:23 -0600

    pass global variable to locals

--- Class practice/Practice section 4/scopes2.py
+++ Class practice/Practice section 4/scopes2.py
@@ -1,12 +1,13 @@
 name = "Name1"
 def print_name_box():
+    global name
     print(name)
     name = "Name2"
     def smaller_box():
         name = "Name3"
         print(name)
         def smallest_box():
-            name = "Name3"
+            name = "Name333"
             print(name)
         smallest_box()
     smaller_box()
