e3000d955400236ec4ce94caf897340183bf3d4c
Uluru92 <jorddy.castro@gmail.com>
2024-06-21 14:52:43 -0600

    call out a def

--- Class practice/Practice section 4/example_class.py
+++ Class practice/Practice section 4/example_class.py
@@ -7,5 +7,13 @@ def hello():
 print(random.randint(0, 100))
 hello()
 
+# how to call a def with different inputs
 
+def greet(greeting, name):
+    sentence = f"{greeting}, {name}! How are you?"
+    return sentence
 
+print(greet("Hello", "World"))  # Hello, World! How are you?
+print(greet("Howdy", "partner"))  # Howdy, partner! How are you?
+print(greet("Zdravo", "prijatelj"))  # Zdravo, prijatelj! How are you?
+print(greet("Hi", "Martin"))  # Hi, Martin! How are you?
