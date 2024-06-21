ecdae8b4b5da1d5ce9aad77756d03c996cb3b64e
Uluru92 <jorddy.castro@gmail.com>
2024-06-21 09:35:08 -0600

    print lyrics def

--- Class practice/Practice section 4/function_syntax.py
+++ Class practice/Practice section 4/function_syntax.py
@@ -1,3 +1,11 @@
 def my_func(parameter):
     pass # Replace with code that does something
     return  # Followed by a value that the function gives back
+
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+print_lyrics()
+
