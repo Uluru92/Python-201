15fb8a9a6cb378b840a3e9c1a6764e1e5820be0c
Uluru92 <jorddy.castro@gmail.com>
2024-06-21 12:45:11 -0600

    printing fruitful and void return values

---
+++ Class practice/Practice section 4/practice_return_values.py
@@ -0,0 +1,9 @@
+a = print("Helloooo!")
+b = int(3)
+c = str(4)
+d = list[1,2,3]
+
+print(a) #fruitful function bc returns a value
+print(b) #void function bc returns None
+print(c) #fruitful function bc returns a value
+print(d) #fruitful function bc returns a value
