978688d193714e24ea990bcd757439b6d52d8116
Uluru92 <jorddy.castro@gmail.com>
2024-06-21 16:34:36 -0600

    pass arguments with parameter names

--- Class practice/Practice section 4/example_parameters_def.py
+++ Class practice/Practice section 4/example_parameters_def.py
@@ -4,7 +4,17 @@ def greet(greeting, name):
     sentence = f"{greeting}, {name}! How are you?"
     return sentence
 
+#Order matters by you can uou can also pass arguments
+#together with their parameter names as keyword arguments
+
 print(greet("Hello", "Jorddy"))  # Hello, World! How are you?
-print(greet("Howdy", "partner"))  # Howdy, partner! How are you?
+print(greet(name="Jorddy", greeting="Hello"))
+
+print(greet("Howdy", 777))  # Howdy, partner! How are you?
+print(greet(name=888, greeting="Howdy"))
+
 print(greet("Zdravo", "prijatelj"))  # Zdravo, prijatelj! How are you?
+print(greet(name="prijatelj", greeting="Zdravo"))
+
 print(greet("Hi", "Martin"))  # Hi, Martin! How are you?
+print(greet(name="Martin", greeting="Hi"))
