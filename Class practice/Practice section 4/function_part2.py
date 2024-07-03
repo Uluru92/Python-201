39dd08773b6e75c089750957e8d1fbd40b3758e0
Uluru92 <jorddy.castro@gmail.com>
2024-07-03 16:56:43 -0600

    practice parts of a function!

---
+++ Class practice/Practice section 4/function_part2.py
@@ -0,0 +1,18 @@
+def greet(greeting, name):
+    sentence = f"{greeting}, {name}! How are you?"
+    print(sentence)
+
+
+def greet2(greeting, name):
+    sentence = f"{greeting}, {name}! How are you old friend?"
+    print(sentence)
+    return sentence
+
+def void_func(message):
+    print(message)
+
+
+
+saludando = greet("Hey","Be")
+saludando = greet2("Hi","Pat")
+void_func("hola!")
