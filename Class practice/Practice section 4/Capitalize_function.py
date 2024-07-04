891e1cb1248e9e8d8ec070caefb63e4a69c0ae64
Uluru92 <jorddy.castro@gmail.com>
2024-07-04 16:21:36 -0600

    already builds functions: tittle... capitalize...

--- Class practice/Practice section 4/Capitalize_function.py
+++ Class practice/Practice section 4/Capitalize_function.py
@@ -1,4 +1,6 @@
 sentence = "lets capitalize every word in this sentence"
+sentence2 = "i love python"
+sentence3 = "quiero tomar cafe con pan"
 
 def cap_words (text):
     healine_word = []
@@ -7,6 +9,10 @@ def cap_words (text):
         healine_word.append(cap_word)
     return " ".join(healine_word)
 
-capitalized_test = cap_words(sentence)
-print(capitalized_test)
+capitalized_sentence = cap_words(sentence)
+print("sentence:", sentence)
+print("capitalized_sentence:", capitalized_sentence)
+
+print(sentence2.title())
+print(sentence3.capitalize())
 
