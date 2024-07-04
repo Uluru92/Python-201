b831297c852f942179f84602d7f69eb52650e352
Uluru92 <jorddy.castro@gmail.com>
2024-07-04 17:39:05 -0600

    args simple asterisk practice

---
+++ Class practice/Practice section 4/args_simple_asterisk.py
@@ -0,0 +1,11 @@
+def print_args(*args):
+    for a in args:
+        print(a)
+
+print_args("apta 1", "apta 2", "apta 3", "apta 4", "apta 5", "airbnb")
+
+# OUTPUT:
+# barcelona
+# tahoe
+# ubud
+# koh tao
