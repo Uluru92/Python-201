d0114eb2cca458362f00884d633ae74c42e527bb
Uluru92 <jorddy.castro@gmail.com>
2024-07-04 17:42:13 -0600

    args names... is just a variable! but stick with args!

--- Class practice/Practice section 4/args_simple_asterisk.py
+++ Class practice/Practice section 4/args_simple_asterisk.py
@@ -1,6 +1,7 @@
-def print_args(*args):
-    for a in args:
-        print(a)
+def print_args(*argumentos): #you can use any variable name...  
+    for a in argumentos:     #but...for everyone's sake, just stick with args
+        print(a)             #to help others and yourself remember 
+                             #the language feature you're using.
 
 print_args("apta 1", "apta 2", "apta 3", "apta 4", "apta 5", "airbnb")
 
