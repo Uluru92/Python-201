dda7d1d40a77f776e012952100668204781814ea
Uluru92 <jorddy.castro@gmail.com>
2024-07-26 15:24:30 -0600

    alternative enumerate function

--- Class_practice/Practice_section_4/enumerate.py
+++ Class_practice/Practice_section_4/enumerate.py
@@ -1,4 +1,10 @@
+#Enumerate function
 siblings = ["Maritza","Melvin","Mirna","Maribel","Sandra","Yadira","Javier","Dixie"]
-
 for index, person in enumerate(siblings, start=1):
     print(f"Sibling #{index}: {person}")
+
+    
+#Alternative
+courses = ["Maritza","Melvin","Mirna","Maribel","Sandra","Yadira","Javier","Dixie"]
+for index in range(len(courses)):
+    print(f"Sibling #{index+1}: {courses[index]}")
