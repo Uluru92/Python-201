bb1872e97af047c87c44a0e81227aec9d08f84af
Uluru92 <jorddy.castro@gmail.com>
2024-08-22 20:25:04 -0600

    different import ways

--- Class_practice/Practice_section_6/ingredients.py
+++ Class_practice/Practice_section_6/ingredients.py
@@ -1,1 +1,5 @@
 ingredient_1 = "carrot"
+ingredient_2 = "potatoes"
+ingredient_3 = "chicken"
+ingredient_4 = "nuddles"
+ingredient_5 = "salt"
--- Class_practice/Practice_section_6/soup.py
+++ Class_practice/Practice_section_6/soup.py
@@ -1,3 +1,16 @@
+#import #1
 from ingredients import ingredient_1
 
 print(ingredient_1)
+
+#import #2
+import ingredients
+
+print(ingredients.ingredient_2)
+print(ingredients.ingredient_3)
+
+#import #3
+import ingredients as i
+print(i.ingredient_4)
+print(i.ingredient_5)
+
