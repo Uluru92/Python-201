9993694c589ebe45e48bd8e3d73d2ef36202f916
Uluru92 <jorddy.castro@gmail.com>
2024-09-13 18:21:18 -0600

    exercise 6.13 DONE

---
+++ 06_advanced-python-concepts/06_13_void_return_DONE.py
@@ -0,0 +1,9 @@
+# Write a lambda function that does not take in an arguments 
+# but returns a value. Print the return value.
+
+#solution
+
+y = "playing around with this lambda thing"
+x = "Me"
+writing_something = lambda: x+" "+y
+print(writing_something())
