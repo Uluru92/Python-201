aa286e744705a70daf4f009e2029af772ad49610
Uluru92 <jorddy.castro@gmail.com>
2024-09-13 18:24:44 -0600

    exercise 6.14 DONE

--- 06_advanced-python-concepts/06_14_hello_lambda_DONE.py
+++ 06_advanced-python-concepts/06_14_hello_lambda_DONE.py
@@ -1,2 +1,7 @@
 # Create a lambda expression that takes no input
 # and prints "hello world" to the console. What does it return?
+
+#solution
+
+lambda_expresion = lambda: print("hello world")
+lambda_expresion() #it does return "hello world"
