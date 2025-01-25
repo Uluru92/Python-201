abd4d0e308198607ce65c7cdd213e0a006af775b
Uluru92 <jorddy.castro@gmail.com>
2025-01-24 18:30:10 -0600

    using database throught sqlalchemy extracting actor information throught table

--- Class_practice/Practice_section_7/Env/SQLAlchemy_venv_practice/SQLAlchemy_practice.py
+++ Class_practice/Practice_section_7/Env/SQLAlchemy_venv_practice/SQLAlchemy_practice.py
@@ -1,9 +1,20 @@
-import sqlalchemy  
+import sqlalchemy
+from Examples.actor import Actor
   
-engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/sakila')  
-connection = engine.connect()  
+engine = sqlalchemy.create_engine('mysql+pymysql://root:Coloresl2l2!@localhost/sakila')  
+connection = engine.connect()
 metadata = sqlalchemy.MetaData()  
-actor = sqlalchemy.Table('actor', metadata, autoload_with=engine)  
+
+actor = sqlalchemy.Table('actor', metadata, autoload_with=engine)
   
-print(actor.columns.keys())  
-print(repr(metadata.tables['actor']))
+query = sqlalchemy.select(actor)
+result_proxy = connection.execute(query)
+
+actor_list = []
+  
+for result in result_proxy:
+    new_actor = Actor(result[0], result[1], result[2], result[3])
+    actor_list.append(new_actor)
+
+for actor in actor_list:
+    print(actor)
