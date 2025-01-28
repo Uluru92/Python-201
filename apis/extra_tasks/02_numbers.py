13e3b65be00401d1202ea0dd6bc30c6f6c19a962
Uluru92 <jorddy.castro@gmail.com>
2025-01-28 16:00:34 -0600

    ingest API data from at least 1 external source DONE

--- apis/extra_tasks/02_numbers.py
+++ apis/extra_tasks/02_numbers.py
@@ -11,3 +11,23 @@ however it needs to fulfill the following specs:
 TIP: consider using a cryptocurrency API such as coinmarketcap (but anything goes)!
 
 '''
+#FIRST STEP: ingest API data from at least 1 external source
+import requests
+from pprint import pprint
+
+base_url = "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+
+headers = {
+    'Accepts': 'application/json',
+    "X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
+}
+
+response = requests.get(base_url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print("\nDATA:\n")
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
+    print(response.json())
