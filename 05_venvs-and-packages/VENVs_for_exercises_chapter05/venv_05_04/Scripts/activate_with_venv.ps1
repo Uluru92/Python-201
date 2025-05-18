51eac416f56068553b8cd1a5b4ef35f7fec35e85
Uluru92 <jorddy.castro@gmail.com>
2025-05-18 00:53:19 -0600

    exercise 05_04 DONE

---
+++ 05_venvs-and-packages/VENVs_for_exercises_chapter05/venv_05_04/Scripts/activate_with_venv.ps1
@@ -0,0 +1,6 @@
+# Activa el entorno
+& "C:\Users\User\Documents\TI\CodingNomads_2024\Python-201\Python-201-main\05_venvs-and-packages\VENVs_for_exercises_chapter05\venv_05_04\Scripts\Activate.ps1"
+
+# Exporta variables
+$env:ENVIRONMENT = "development"
+$env:SECRET = "i ate your sweets"
