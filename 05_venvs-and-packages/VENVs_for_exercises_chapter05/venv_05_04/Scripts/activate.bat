d947257b1bdc70629c6240f18680466ff6c0d543
Uluru92 <jorddy.castro@gmail.com>
2025-06-12 21:46:54 -0600

    mejora ejercicios chapter 5

--- 05_venvs-and-packages/VENVs_for_exercises_chapter05/venv_05_04/Scripts/activate.bat
+++ 05_venvs-and-packages/VENVs_for_exercises_chapter05/venv_05_04/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\User\Documents\TI\CodingNomads_2024\Python-201\Python-201-main\05_venvs-and-packages\VENVs_for_exercises_chapter05\venv_05_04"
+set "VIRTUAL_ENV=C:\Users\jordd\Documents\Repositorios Github\Python-201\05_venvs-and-packages\VENVs_for_exercises_chapter05\venv_05_04"
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(venv_05_04) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(venv_05_04) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,13 +25,10 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
-set "VIRTUAL_ENV_PROMPT=(venv_05_04) "
+set "VIRTUAL_ENV_PROMPT=venv_05_04"
 
 :END
 if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
     set _OLD_CODEPAGE=
 )
-
-set ENVIRONMENT=development
-set SECRET=i ate your sweets
--- 05_venvs-and-packages/VENVs_for_exercises_chapter05/venv_05_04/Scripts/activate_with_venv.ps1
+++
@@ -1,6 +0,0 @@
-# Activa el entorno
-& "Python-201\05_venvs-and-packages\VENVs_for_exercises_chapter05\venv_05_04\Scripts\Activate.ps1"
-
-# Exporta variables
-$env:ENVIRONMENT = "development"
-$env:SECRET = "i ate your sweets"
