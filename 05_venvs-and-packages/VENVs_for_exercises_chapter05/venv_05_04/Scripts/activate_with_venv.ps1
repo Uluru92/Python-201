f323e7f021345cd9136bc2ad048b934f3cc20618
Uluru92 <jorddy.castro@gmail.com>
2025-06-12 21:27:51 -0600

    path name changed

--- 05_venvs-and-packages/VENVs_for_exercises_chapter05/venv_05_04/Scripts/activate_with_venv.ps1
+++ 05_venvs-and-packages/VENVs_for_exercises_chapter05/venv_05_04/Scripts/activate_with_venv.ps1
@@ -1,5 +1,5 @@
 # Activa el entorno
-& "C:\Users\jordd\Documents\Repositorios Github\Python-201\05_venvs-and-packages\VENVs_for_exercises_chapter05\venv_05_04\Scripts\Activate.ps1"
+& "Python-201\05_venvs-and-packages\VENVs_for_exercises_chapter05\venv_05_04\Scripts\Activate.ps1"
 
 # Exporta variables
 $env:ENVIRONMENT = "development"
