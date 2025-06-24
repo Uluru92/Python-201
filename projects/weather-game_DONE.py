a5b4fd8286945f4c3499bc25e2607703c324027a
Uluru92 <jorddy.castro@gmail.com>
2025-06-24 00:40:18 -0600

    api weather implemented

---
+++ projects/weather-game_DONE.py
@@ -0,0 +1,8 @@
+# Include the current weather into your game mechanics.
+
+URL = "https://www.metaweather.com/api/" # it doesn't work, I couldnt find a weather API without key requiremnet...
+
+# My solution is implemented in the Adventure_Game: python_miniprojects\Adventure_Game_DONE.py
+# I used the following url: https://www.meteosource.com/api/v1/free/point?place_id=london&sections=all&timezone=UTC&language=en&units=metric&key={api_key}
+# Please use your own key or ask for mine, I prefer not to upload my key to Github :) for good practices!
+
