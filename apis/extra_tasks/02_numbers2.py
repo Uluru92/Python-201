5b69bca9d7bd7e37b8e0f313050a2bd743b966ab
Uluru92 <jorddy.castro@gmail.com>
2025-07-01 21:58:59 -0600

    exercise 02_numbers API continue

--- apis/extra_tasks/02_numbers2.py
+++ apis/extra_tasks/02_numbers2.py
@@ -14,21 +14,31 @@ TIP: consider using a cryptocurrency API such as coinmarketcap (but anything goe
 # Solution
 #   Pseudocode
 #       Topic: Currencies
-#       GET: extract all the symbols from API
-
-#FIRST STEP: ingest API data from at least 1 external source
+#       GET: extract company's symbols, API https://finnhub.io/api/v1/stock/symbol?exchange=US&token={api_key}
+#       GET: extract recomendations for every company, API https://finnhub.io/api/v1/stock/recommendation?symbol=GWH&token={api_key}
+#
+# FIRST STEP: ingest API data from at least 1 external source
 
 import requests
 import os
 from pprint import pprint
 from dotenv import load_dotenv
 
+# API resources:
 load_dotenv()
-
 api_key = os.getenv("API_FINNHUB")
+url_companies = f"https://finnhub.io/api/v1/stock/symbol?exchange=US&token={api_key}"
+url_recomendations = f"https://finnhub.io/api/v1/stock/recommendation?symbol=GWH&token={api_key}"
 
-url = f"https://finnhub.io/api/v1/stock/recommendation?symbol=GWH&token={api_key}"
+# Data received:
+data_finnhub_companies = requests.get(url_companies).json()
 
-data_finnhub = requests.get(url).json()
+# Create a list of every company's symbol. This is a REQUIRED argument to get latest analyst recommendation trends for a company.
+symbols = [item["displaySymbol"] for item in data_finnhub_companies]
 
-pprint(data_finnhub)
+# Use our required argument list, to ingest API data
+for index, company in enumerate(data_finnhub_companies):
+    symbol = company["displaySymbol"]
+    if symbol in symbols:
+        print(f"{index}: Procesando {symbol} - {company['description']}")
+
