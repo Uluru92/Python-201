c8e83593c90509530b9472089b34ffb74e94de18
Uluru92 <jorddy.castro@gmail.com>
2025-07-24 15:38:08 -0600

    to be continue...

--- apis/extra_tasks/09_tweet.py
+++ apis/extra_tasks/09_tweet.py
@@ -9,3 +9,32 @@ BONUS: Look into CRON jobs to automate your tweets to go out at scheduled times.
        E.g.: "Don't start without me, I'm nearly there!" every weekday at 9:14... ;P
 
 '''
+
+import tweepy
+import json
+import random
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = tweepy.Client(
+    consumer_key=os.getenv("API_tweepy_consumer_key"),
+    consumer_secret=os.getenv("API_tweepy_consumer_secret"),
+    access_token=os.getenv("API_tweepy_access_token"),
+    access_token_secret=os.getenv("API_tweepy_access_token_secret")
+)
+
+def post_random_tweet():
+    with open(, "r", encoding="utf-8") as f:
+        tweets = json.load(f)
+    tweet = random.choice(tweets)
+
+    try:
+        client.create_tweet(text=tweet)
+        print(f"✅ Tweet posted: {tweet}")
+    except Exception as e:
+        print(f"❌ Error posting tweet: {e}")
+
+if __name__ == "__main__":
+    post_random_tweet()
